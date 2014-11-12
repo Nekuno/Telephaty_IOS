4bb3bdb5585ef07eccc5977220dea6105752c6c4
Pedro Muñoz Cabrera <pmunoz08@gmail.com>
2014-11-12 19:51:11 +0100

    New class to hold all library configurations

---
+++ telephaty/KNConfigurationService.h
@@ -0,0 +1,13 @@
+//
+//  KNConfigurationService.h
+//  Telephaty
+//
+//  Created by PEDRO MUÑOZ CABRERA on 12/11/14.
+//  Copyright (c) 2014 kiliannet. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KNConfigurationService : NSObject
+
+@end

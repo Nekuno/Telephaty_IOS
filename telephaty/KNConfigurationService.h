8e7977009439e2c54fc1ed2a38d00a237fe6558d
Pedro Muñoz Cabrera <pmunoz08@gmail.com>
2014-11-12 20:45:29 +0100

    Added macros for configuration

--- telephaty/KNConfigurationService.h
+++ telephaty/KNConfigurationService.h
@@ -8,6 +8,10 @@
 
 #import <Foundation/Foundation.h>
 
+#define TELEPHATY_SERVICE_UUID              @"00001101-0000-1000-8000-00805F9B34FB"
+#define TELEPHATY_CHARACTERISTIC_UUID       @"00001101-0000-1000-8000-00805F9B34FA"
+#define REMOVE_MESSAGES_OLDER_THAN_MINUTES  15
+
 @interface KNConfigurationService : NSObject
 
 @end

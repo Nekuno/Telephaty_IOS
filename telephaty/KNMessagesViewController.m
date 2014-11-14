eeef6851e0db537d53ddd002247c863dc3054a61
Pedro Muñoz Cabrera <pmunoz08@gmail.com>
2014-11-14 09:56:41 +0100

    Clean sample from no needed comments and code

--- telephaty/KNMessagesViewController.m
+++ telephaty/KNMessagesViewController.m
@@ -76,9 +76,6 @@ static const NSInteger kAlertRemoveAllMessages    = 80;
   
   /**
    *  Create message bubble images objects.
-   *
-   *  Be sure to create your bubble images one time and reuse them for good performance.
-   *
    */
   JSQMessagesBubbleImageFactory *bubbleFactory = [[JSQMessagesBubbleImageFactory alloc] init];
   
@@ -99,13 +96,6 @@ static const NSInteger kAlertRemoveAllMessages    = 80;
   [[NSNotificationCenter defaultCenter] removeObserver:self name:kNotificationRemovedOldMessages object:nil];
 }
 
-- (void)viewDidAppear:(BOOL)animated {
-  [super viewDidAppear:animated];
-  
-  // Enable/disable springy bubbles
-//  self.collectionView.collectionViewLayout.springinessEnabled = YES;
-}
-
 - (void)didReceiveMemoryWarning {
   [super didReceiveMemoryWarning];
 }
@@ -117,13 +107,7 @@ static const NSInteger kAlertRemoveAllMessages    = 80;
                   senderId:(NSString *)senderId
          senderDisplayName:(NSString *)senderDisplayName
                       date:(NSDate *)date {
-  /**
-   *  Sending a message. Your implementation of this method should do *at least* the following:
-   *
-   *  1. Play sound (optional)
-   *  2. Add new id<JSQMessageData> object to your data source
-   *  3. Call `finishSendingMessage`
-   */
+
   [JSQSystemSoundPlayer jsq_playMessageSentSound];
   
   JSQMessage *message = [[JSQMessage alloc] initWithSenderId:senderId
@@ -148,12 +132,6 @@ static const NSInteger kAlertRemoveAllMessages    = 80;
 
 - (id<JSQMessageBubbleImageDataSource>)collectionView:(JSQMessagesCollectionView *)collectionView
              messageBubbleImageDataForItemAtIndexPath:(NSIndexPath *)indexPath {
-  /**
-   *  You may return nil here if you do not want bubbles.
-   *  In this case, you should set the background color of your collection view cell's textView.
-   *
-   *  Otherwise, return your previously created bubble image data objects.
-   */
   
   JSQMessage *message = [self.messages objectAtIndex:indexPath.item];
   
@@ -164,84 +142,16 @@ static const NSInteger kAlertRemoveAllMessages    = 80;
   return self.incomingBubbleImageData;
 }
 
-- (id<JSQMessageAvatarImageDataSource>)collectionView:(JSQMessagesCollectionView *)collectionView
-                    avatarImageDataForItemAtIndexPath:(NSIndexPath *)indexPath {
-  /**
-   *  Return `nil` here if you do not want avatars.
-   *  If you do return `nil`, be sure to do the following in `viewDidLoad`:
-   *
-   *  self.collectionView.collectionViewLayout.incomingAvatarViewSize = CGSizeZero;
-   *  self.collectionView.collectionViewLayout.outgoingAvatarViewSize = CGSizeZero;
-   *
-   *  It is possible to have only outgoing avatars or only incoming avatars, too.
-   */
-  
-  /**
-   *  Return your previously created avatar image data objects.
-   *
-   *  Note: these the avatars will be sized according to these values:
-   *
-   *  self.collectionView.collectionViewLayout.incomingAvatarViewSize
-   *  self.collectionView.collectionViewLayout.outgoingAvatarViewSize
-   *
-   *  Override the defaults in `viewDidLoad`
-   */
-//  JSQMessage *message = [self.demoData.messages objectAtIndex:indexPath.item];
-//  
-//  if ([message.senderId isEqualToString:self.senderId]) {
-//    if (![NSUserDefaults outgoingAvatarSetting]) {
-//      return nil;
-//    }
-//  }
-//  else {
-//    if (![NSUserDefaults incomingAvatarSetting]) {
-//      return nil;
-//    }
-//  }
-//  
-//  
-//  return [self.demoData.avatars objectForKey:message.senderId];
-  return nil;
-}
 
 - (NSAttributedString *)collectionView:(JSQMessagesCollectionView *)collectionView
 attributedTextForCellTopLabelAtIndexPath:(NSIndexPath *)indexPath {
-  /**
-   *  This logic should be consistent with what you return from `heightForCellTopLabelAtIndexPath:`
-   *  The other label text delegate methods should follow a similar pattern.
-   *
-   *  Show a timestamp for every 3rd message
-   */
+
   if (indexPath.item % 3 == 0) {
     JSQMessage *message = [self.messages objectAtIndex:indexPath.item];
     return [[JSQMessagesTimestampFormatter sharedFormatter] attributedTimestampForDate:message.date];
   }
   
   return nil;
-}
-
-- (NSAttributedString *)collectionView:(JSQMessagesCollectionView *)collectionView
-attributedTextForMessageBubbleTopLabelAtIndexPath:(NSIndexPath *)indexPath {
-  JSQMessage *message = [self.messages objectAtIndex:indexPath.item];
-  
-  /**
-   *  iOS7-style sender name labels
-   */
-  if ([message.senderId isEqualToString:self.senderId]) {
-    return nil;
-  }
-  
-  if (indexPath.item - 1 > 0) {
-    JSQMessage *previousMessage = [self.messages objectAtIndex:indexPath.item - 1];
-    if ([[previousMessage senderId] isEqualToString:message.senderId]) {
-      return nil;
-    }
-  }
-  
-  /**
-   *  Don't specify attributes to use the defaults.
-   */
-  return [[NSAttributedString alloc] initWithString:message.senderDisplayName];
 }
 
 - (NSAttributedString *)collectionView:(JSQMessagesCollectionView *)collectionView
@@ -258,24 +168,8 @@ attributedTextForCellBottomLabelAtIndexPath:(NSIndexPath *)indexPath {
 
 - (UICollectionViewCell *)collectionView:(JSQMessagesCollectionView *)collectionView
                   cellForItemAtIndexPath:(NSIndexPath *)indexPath {
-  /**
-   *  Override point for customizing cells
-   */
+
   JSQMessagesCollectionViewCell *cell = (JSQMessagesCollectionViewCell *)[super collectionView:collectionView cellForItemAtIndexPath:indexPath];
-  
-  /**
-   *  Configure almost *anything* on the cell
-   *
-   *  Text colors, label text, label colors, etc.
-   *
-   *
-   *  DO NOT set `cell.textView.font` !
-   *  Instead, you need to set `self.collectionView.collectionViewLayout.messageBubbleFont` to the font you want in `viewDidLoad`
-   *
-   *
-   *  DO NOT manipulate cell layout information!
-   *  Instead, override the properties you want on `self.collectionView.collectionViewLayout` from `viewDidLoad`
-   */
   
   JSQMessage *msg = [self.messages objectAtIndex:indexPath.item];
   
@@ -302,16 +196,7 @@ attributedTextForCellBottomLabelAtIndexPath:(NSIndexPath *)indexPath {
 - (CGFloat)collectionView:(JSQMessagesCollectionView *)collectionView
                    layout:(JSQMessagesCollectionViewFlowLayout *)collectionViewLayout
 heightForCellTopLabelAtIndexPath:(NSIndexPath *)indexPath {
-  /**
-   *  Each label in a cell has a `height` delegate method that corresponds to its text dataSource method
-   */
-  
-  /**
-   *  This logic should be consistent with what you return from `attributedTextForCellTopLabelAtIndexPath:`
-   *  The other label height delegate methods should follow similarly
-   *
-   *  Show a timestamp for every 3rd message
-   */
+
   if (indexPath.item % 3 == 0) {
     return kJSQMessagesCollectionViewCellLabelHeightDefault;
   }
